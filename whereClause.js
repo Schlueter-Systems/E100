39719de5bc88be7e359e52841aa48a1f37b7a395
Babu Satuluri <babu_satuluri@schlueter.de>
2017-03-02 15:34:36 +0100

    test2

--- whereClause.js
+++ whereClause.js
@@ -3,3 +3,5 @@ import("lib_grant");
 // Leserechte holen
 a.rs( getGrantCondition( a.valueof("$sys.currentimagenamewithsuffixandallyouneedaddionally"), 
     "( RELATION.ORG_ID = ORG.ORGID and RELATION.PERS_ID is null and ADDRESS.ADDRESSID = RELATION.ADDRESS_ID )" ));
+
+//test
