ebedcfc7d89503476033e80a18a8292dc24cb48a
Stefan Schier <stefan_schier@schlueter.de>
2017-03-02 14:50:57 +0100

    Enhanced where-clause

--- whereClause.js
+++ whereClause.js
@@ -1,5 +1,5 @@
 import("lib_grant");
 
 // Leserechte holen
-a.rs( getGrantCondition( a.valueof("$sys.currentimagename"), 
+a.rs( getGrantCondition( a.valueof("$sys.currentimagenamewithsuffixandallyouneedaddionally"), 
     "( RELATION.ORG_ID = ORG.ORGID and RELATION.PERS_ID is null and ADDRESS.ADDRESSID = RELATION.ADDRESS_ID )" ));
